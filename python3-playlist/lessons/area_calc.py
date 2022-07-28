d255acd37bf19c58ff6871ae8751bf274e1fab96
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-07-28 13:35:12 -0700

    area-calc

---
+++ python3-playlist/lessons/area_calc.py
@@ -0,0 +1,9 @@
+# commented item
+# name = input('Tell me your name:')
+# age = input('...and your age:')
+# print(name,'you are',age)
+
+# Calc the area of a circle
+radius = input('Enter a radius of your circle:')
+area = 3.142 * int(radius)**2
+print('The area of your circle is',area)
